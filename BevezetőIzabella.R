dbee71721096561cfd12231e675b915fcd58a57e
Péter Kalicz <kaliczp@gmail.com>
2025-10-15 13:59:41 +0200

    Beolvasás

--- BevezetőIzabella.R
+++ BevezetőIzabella.R
@@ -2,4 +2,16 @@
 dir() # Ellenőrzöm, hogy sikerüólt-e a másolás
 install.packages("readxl") # Telepítés
 library(readxl) # Csomag betöltése
-search()
+search() # betöltött csomagok (keresési útvonal)
+ls() # munkakörnyezetben szereplő objektumok
+Galand <- as.data.frame(read_excel("Galandfereg.xlsx",4)) # A munkalap megnevezésével
+str(Galand) # adatok áttekintése
+Galand$Faj # Változó kiválasztása
+Galand$"Hullatek faj" # Szóközzel az oszlopnév idézőjelbe
+factor(Galand$Faj) # Faktorrá alakítás
+Galand$Faj <- factor(Galand$Faj) # A faktor mentése az adatbázisba
+str(Galand) # Ellenőrzés
+summary(Galand)
+summary(Galand$Faj)
+summary(Galand$darab)
+summary(Galand$Homérséklet)
