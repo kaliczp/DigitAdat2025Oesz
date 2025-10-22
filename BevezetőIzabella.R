4d4baccc22a65e545f5b8d55d42d212465531c35
Péter Kalicz <kaliczp@gmail.com>
2025-10-22 14:08:19 +0200

    Normális eloszlás grafikusan is

--- BevezetőIzabella.R
+++ BevezetőIzabella.R
@@ -18,6 +18,22 @@ summary(Galand$Homérséklet)
 ## A numerikus változó esetében a summary dolgainak megjelenítése
 boxplot(Galand$Homérséklet)
 quantile(Galand$Homérséklet) # kvantilisek a boxplothoz
+## Boxplot dobozának szélessége az inter-kvartilis terjedelem
+IQR(Galand$Homérséklet)
+## Normalitáshoz Q-Q plot
+qqnorm(Galand$Homérséklet)
+qqline(Galand$Homérséklet) # alsó- felső kvartilisen átmenő egyenes
+hist(Galand$Homérséklet) # Hisztogram elemszám
+hist(Galand$Homérséklet, freq = FALSE) # területe = 1
+lines(density(Galand$Homérséklet), lwd = 2) # Sűrűség fv. becslése
+## Ha bizonytalan vagyok érdemes megnézni,
+## hogy hasonló méretű normális eloszlású adatsor hogyan néz ki?
+GeneraltNorm <- rnorm(nrow(Galand))
+hist(GeneraltNorm, freq = FALSE) # hisztogram alapján is látszik, hogy szép szimmetrikus
+lines(density(GeneraltNorm), lwd = 2) # Sűrűség fv. becslése
+
+qqnorm(GeneraltNorm)
+qqline(GeneraltNorm)
 
 
 ## Megjelenési dinamika és mi hat rá. Időben, évszakban, hónapban.
