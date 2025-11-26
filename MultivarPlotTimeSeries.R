a3297fcfc1baaeb761da908d8cd2a1a9ec62371a
Péter Kalicz <kaliczp@gmail.com>
2025-11-26 14:57:43 +0100

    Grid hátul, csapadék hozzáadva

--- MultivarPlotTimeSeries.R
+++ MultivarPlotTimeSeries.R
@@ -7,8 +7,12 @@ Tes202504.xts <- xts(Tes202504[,-(1:2)], Tes202504[,2])
 ## Ábra gyártás
 par(las=1)
 plot.zoo(Tes202504.xts['2025-04-13/2025-04-22',4],
-         col="red", lwd = 2,
+         type = "n",
          ylim = c(0,22),
          xlab = "", ylab = "Temperature ďegC",
          xaxs="i", yaxs="i")
-grid()
+grid(nx = NA, ny = NULL)
+lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',4]),
+      col="red", lwd = 2)
+lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',3]),
+      col="blue", lwd = 2, type = "h")
