89f2172694731cce3d40cd2a552334fb801d7ecc
Péter Kalicz <kaliczp@gmail.com>
2025-11-26 15:08:11 +0100

    Másik két változóval felülplottolás

--- MultivarPlotTimeSeries.R
+++ MultivarPlotTimeSeries.R
@@ -15,4 +15,15 @@ grid(nx = NA, ny = NULL)
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',4]),
       col="red", lwd = 2)
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',3]),
-      col="blue", lwd = 2, type = "h")
+      lend = 1,
+      col="blue", lwd = 5, type = "h")
+par(new = TRUE)
+plot.zoo(Tes202504.xts['2025-04-13/2025-04-22', 1],
+         col = "orange", lwd = 2,
+         xlab = "", ylab = "Tension",
+         ylim = c(7.6,9.5),
+         xaxs="i", yaxs="i")
+lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22', 2]),
+         col = "green", lwd = 2,
+         ylim = c(7.6,9.5),
+         xaxs="i", yaxs="i")
