645fa6cfdf89afadaf280139a9370b882f3ee43f
Péter Kalicz <kaliczp@gmail.com>
2025-11-26 18:44:09 +0100

    Kb itt ért véget a videó

--- MultivarPlotTimeSeries.R
+++ MultivarPlotTimeSeries.R
@@ -5,13 +5,18 @@ Sys.setenv(TZ="UTC")
 Tes202504.xts <- xts(Tes202504[,-(1:2)], Tes202504[,2])
 
 ## Ábra gyártás
+Sys.setenv(LANG = "en_US.UTF-8")
+
 png("TensileTR202504.png", width= 28.2, height= 6.7, units = "cm", res = 300)
 par(las=1, mar = c(3.1,4.1,0.6,4.1))
 plot.zoo(Tes202504.xts['2025-04-13/2025-04-22',4],
          type = "n",
+         xaxt = "n",
          ylim = c(0,22),
          xlab = "", ylab = "Temperature ďegC",
          xaxs="i", yaxs="i")
+axis.POSIXct(1,at = c(ISOdate(2025,4,13:22,0,0),ISOdate(2025,4,22,23,50)), labels = FALSE)
+axis.POSIXct(1,at = ISOdate(2025,4,13:23), tck = 0, format="%d %b")
 grid(nx = NA, ny = NULL)
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',4]),
       col="red", lwd = 2)
@@ -23,7 +28,7 @@ plot.zoo(Tes202504.xts['2025-04-13/2025-04-22', 1],
          col = "orange", lwd = 2,
          xlab = "", ylab = "",
          ylim = c(7.6,9.5),
-         yaxt = "n",
+         xaxt = "n", yaxt = "n",
          xaxs="i", yaxs="i")
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22', 2]),
          col = "green", lwd = 2,
@@ -31,8 +36,7 @@ lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22', 2]),
          xaxs="i", yaxs="i")
 axis(4)
 mtext("Tension [mA]", 4, 3, las = 0)
-legend("bottomright", legend = c("Temp.","Prec.", "Tens. 1", "Tens. 2"), lwd = c(2,4,2,2),
+legend(ISOdatetime(2025,4,20,8,0,0), y = 8.5, legend = c("Temp.","Prec.", "Tens. 1", "Tens. 2"), lwd = c(2,4,2,2),
+       ncol = 2,
        col = c("red", "blue", "orange", "green"))
 dev.off()
-
-
