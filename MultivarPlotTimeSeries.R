12d20f994324bc5287b0f6bdead4aa9e21140e52
Péter Kalicz <kaliczp@gmail.com>
2025-11-27 11:39:59 +0100

    Visual enhancements
    Hőmérséklet feliratnál fok, tension adatok szinkronban a hőmérséklet griddel, legend feliratai és elhelyezése, mérete javítva

--- MultivarPlotTimeSeries.R
+++ MultivarPlotTimeSeries.R
@@ -13,7 +13,7 @@ plot.zoo(Tes202504.xts['2025-04-13/2025-04-22',4],
          type = "n",
          xaxt = "n",
          ylim = c(0,22),
-         xlab = "", ylab = "Temperature ďegC",
+         xlab = "", ylab = "Temperature [°C]",
          xaxs="i", yaxs="i")
 axis.POSIXct(1,at = c(ISOdate(2025,4,13:22,0,0),ISOdate(2025,4,22,23,50)), labels = FALSE)
 axis(1, at = ISOdate(2025,4,13:22), tck = 0, labels = paste(13:22, "Apr."))
@@ -27,16 +27,15 @@ par(new = TRUE, mar = c(3.1,4.1,0.6,4.1))
 plot.zoo(Tes202504.xts['2025-04-13/2025-04-22', 1],
          col = "orange", lwd = 2,
          xlab = "", ylab = "",
-         ylim = c(7.6,9.5),
+         ylim = c(7.4,9.6),
          xaxt = "n", yaxt = "n",
          xaxs="i", yaxs="i")
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22', 2]),
          col = "green", lwd = 2,
-         ylim = c(7.6,9.5),
          xaxs="i", yaxs="i")
-axis(4)
+axis(4, at = c(7.9,8.4,8.9,9.4))
 mtext("Tension [mA]", 4, 3, las = 0)
-legend(ISOdatetime(2025,4,20,8,0,0), y = 8.5, legend = c("Temp.","Prec.", "Tens. 1", "Tens. 2"), lwd = c(2,4,2,2),
-       ncol = 2,
+legend(ISOdate(2025,4,20), y = 8.5, legend = c("Temp.","Prec.", "Tés 1", "Tés 2"), lwd = c(2,4,2,2),
+       ncol = 2, cex = 0.9,
        col = c("red", "blue", "orange", "green"))
 dev.off()
