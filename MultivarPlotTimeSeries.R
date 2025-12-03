74bf4a614de039d9c1afc7553742ea927f92df0b
Péter Kalicz <kaliczp@gmail.com>
2025-12-03 21:43:51 +0100

    Color y labs

--- MultivarPlotTimeSeries.R
+++ MultivarPlotTimeSeries.R
@@ -18,10 +18,10 @@ plot.zoo(Tes202504.xts['2025-04-13/2025-04-22',4],
 axis.POSIXct(1,at = c(ISOdate(2025,4,13:22,0,0),ISOdate(2025,4,22,23,00)), labels = FALSE)
 axis(1, at = ISOdate(2025,4,13:22), tck = 0, labels = paste(13:22, "Apr."), mgp = c(3,0.5,0) )
 grid(nx = NA, ny = NULL)
-mtext("Temperature", 2, 3, at = 15, las = 0)
-mtext("[°C]", 2, 2, at = 15, las = 0)
-mtext("Precipitation", 2, 3, at = 2.5, las = 0)
-mtext("[mm/hours]", 2, 2, at = 2.5, las = 0)
+mtext("Temperature", 2, 3, at = 15, las = 0, col = "red")
+mtext("[°C]", 2, 2, at = 15, las = 0, col = "red")
+mtext("Precipitation", 2, 3, at = 2.5, las = 0, col = "blue")
+mtext("[mm/hours]", 2, 2, at = 2.5, las = 0, col = "blue")
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',4]),
       col="red", lwd = 2)
 lines(as.zoo(Tes202504.xts['2025-04-13/2025-04-22',3]),
